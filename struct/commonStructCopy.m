0dee9c90503231ee481e717a4b57d47babc4f5f9
Deborah Meyer <deborah.meyer@physchem.uni-freiburg.de>
2015-03-06 14:26:56 +0100

    Added function for joining cascaded structs

---
+++ struct/commonStructCopy.m
@@ -0,0 +1,107 @@
+function structure = commonStructCopy(master,tocopy,varargin)
+% COMMONSTRUCTCOPY Copy struct array contents into another array.
+%
+% All fields (and contents) from "tocopy" get copied into "master".
+% Therefore, this function is particularly useful if you have a "master"
+% that has normally more fields than "tocopy" and if you want to preserve
+% the information in "tocopy".
+%
+% Usage
+%   struct = structcopy(master,tocopy)
+%   struct = structcopy(master,tocopy,<parameter>,<value>)
+%
+%   master     - struct
+%                Master means here master in terms of the available fields,
+%                but NOT in terms of their contents.
+%
+%   tocopy     - struct
+%                The contents of this struct get copied in the struct
+%                "master", and if fields of tocopy don't exist in master,
+%                they will be created.
+%
+%   parameters - key-value pairs (OPTIONAL)
+%
+%                Optional parameters may include:
+%
+%                overwrite - boolean
+%                            This parameter is currently not recognised...
+%                            Default: false
+%   
+
+% Copyright (c) 2012-15, Till Biskup
+% 2015-03-06
+
+if ~nargin
+    help structcopy
+    return;
+end
+
+% Parse input arguments using the inputParser functionality
+try
+    p = inputParser;            % Create inputParser instance.
+    p.FunctionName = mfilename; % Function name in error messages
+    p.KeepUnmatched = true;     % Enable errors on unmatched arguments
+    p.StructExpand = true;      % Enable passing arguments in a structure
+    p.addRequired('master', @(x)isstruct(x));
+    p.addRequired('tocopy', @(x)isstruct(x));
+    p.addParamValue('overwrite',logical(false),@islogical);
+    p.parse(master,tocopy,varargin{:});
+catch exception
+    disp(['(EE) ' exception.message]);
+    return;
+end
+
+if isempty(fieldnames(tocopy))
+    structure = master;
+    return;
+end
+
+[structure,tocopy] = traverse(master,tocopy); %#ok<NASGU>
+
+end
+
+
+function [master,tocopy] = traverse(master,tocopy)
+
+try
+    tocopyFieldNames = fieldnames(tocopy);
+    for k=1:length(tocopyFieldNames)
+        if ~isfield(master,tocopyFieldNames{k})
+            master.(tocopyFieldNames{k}) = tocopy.(tocopyFieldNames{k});
+        elseif length(tocopy.(tocopyFieldNames{k}))>1 && ...
+                isstruct(tocopy.(tocopyFieldNames{k})(1))
+            % Need to add additional field names before handling arrays of
+            % structures - otherwise getting "assignment between dissimilar
+            % structures" error
+            tmpFieldNames = fieldnames(tocopy.(tocopyFieldNames{k}));
+            for tmpFieldName = 1:length(tmpFieldNames)
+                if ~isfield(master.(tocopyFieldNames{k}),...
+                        tmpFieldNames(tmpFieldName))
+                    master.(tocopyFieldNames{k}).(...
+                        tmpFieldNames{tmpFieldName}) = '';
+                end
+            end
+            for idx = 1:length(tocopy.(tocopyFieldNames{k}))
+                if length(master.(tocopyFieldNames{k})) < idx
+                    master.(tocopyFieldNames{k})(idx) = ...
+                        master.(tocopyFieldNames{k})(idx-1);
+                end
+                [master.(tocopyFieldNames{k})(idx),...
+                    tocopy.(tocopyFieldNames{k})(idx)] = ...
+                    traverse(master.(tocopyFieldNames{k})(idx),...
+                    tocopy.(tocopyFieldNames{k})(idx));
+            end
+        elseif isstruct(tocopy.(tocopyFieldNames{k}))
+            [master.(tocopyFieldNames{k}),tocopy.(tocopyFieldNames{k})] = ...
+                traverse(master.(tocopyFieldNames{k}),...
+                tocopy.(tocopyFieldNames{k}));
+        else
+            master.(tocopyFieldNames{k}) = tocopy.(tocopyFieldNames{k});
+        end
+    end
+catch exception
+    disp(tocopyFieldNames{k});
+    throw(exception)
+end
+
+end
