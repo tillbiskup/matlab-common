cd9d9ee13770fd492f7f29c5b30d1e1158c5caf1
Till Biskup <till@till-biskup.de>
2015-09-14 21:15:41 +0200

    Added source of constants, upped version number

--- helper/commonConstants.m
+++ helper/commonConstants.m
@@ -22,12 +22,12 @@ function [constant,unit] = commonConstants(varargin)
 % 2015-09-14
 
 % Define constants
-% Columns: name of constant, value of constant, unit of value
+% Columns: name of constant, value of constant, unit of value, source
 constants = {...
-    'c'   ,299792458,      'm s^-1'; ...
-    'h'   ,6.626070040e-34,'J s'   ; ...
-    'hbar',1.054571800e-34,'J s'   ; ...
-    'mub' ,927.4009994e-26,'J T^-1' ...
+    'c'   ,299792458,      'm s^-1','2014 CODATA'; ...
+    'h'   ,6.626070040e-34,'J s'   ,'2014 CODATA'; ...
+    'hbar',1.054571800e-34,'J s'   ,'2014 CODATA'; ...
+    'mub' ,927.4009994e-26,'J T^-1','2014 CODATA'  ...
     };
 
 % Assign default output
@@ -53,9 +53,9 @@ end
 function printConstants(constants)
 
 fprintf('\n');
-fprintf('Name\tValue\t\tUnit\n')
+fprintf('Name\tValue\t\tUnit\tSource\n')
 for const = 1:size(constants,1)
-    fprintf('%s\t%e\t%s\n',constants{const,:});
+    fprintf('%s\t%e\t%s\t%s\n',constants{const});
 end
 fprintf('\n');
 
