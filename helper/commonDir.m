13acb9f5dbfce55d0f20e7f5838695cf97484b52
Till Biskup <till@till-biskup.de>
2015-11-27 21:26:08 +0100

    commonDir somewhat operable

--- helper/commonDir.m
+++ helper/commonDir.m
@@ -1,6 +1,6 @@
 function list = commonDir(pattern,varargin)
 % COMMONDIR Advanced version of the Matlab(r) "dir" command working
-% recursively and hiding hidden files by default.
+% recursively and omitting hidden files by default.
 %
 % Usage
 %   list = commonDir(pattern)
@@ -10,6 +10,14 @@ function list = commonDir(pattern,varargin)
 %
 %   list    - cell array of strings
 %             list of file names
+%
+% NOTE: Currently, only the two special directories "." and ".." are
+%       omitted from the list (for good reasons). Further development
+%       should introduce optional switches for a much finer control of what
+%       shall be displayed and whether to traverse into subdirectories (and
+%       possibly into what depth).
+%
+% SEE ALSO: dir
 
 % Copyright (c) 2015, Till Biskup
 % 2015-11-27
@@ -34,11 +42,22 @@ end
 files = dir(pattern);
 
 for iFile = length(files):-1:1
+    % Remove special directories "." and ".."
     if any(strcmp(files(iFile).name,{'.','..'}))
         files(iFile) = [];
     end
+    % Traverse into subdirectory
+    if files(iFile).isdir
+        sublist = commonDir(files(iFile).name);
+        sublist = cellfun(@(x)fullfile(files(iFile).name,x),sublist,...
+            'UniformOutput',false);
+        list = [list sublist];
+    else
+        list{end+1} = files(iFile).name;
+    end
 end
 
-list = {files.name};
+% Revert sequence of entries
+list = fliplr(list);
 
 end
